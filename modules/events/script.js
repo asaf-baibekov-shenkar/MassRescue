580efe2c28db29c7da3e0e45a48031ab35b8ffe0
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-04-23 14:16:26 +0300

    move html scripts to script js file

--- modules/events/script.js
+++ modules/events/script.js
@@ -8,6 +8,23 @@ $(function() {
 		});
 });
 
+function editButtons() {
+	let edit = document.getElementsByClassName('editButton');
+	let remove = document.getElementsByClassName('removeButton');
+	if (remove[0].hasAttribute("hidden"))
+		for (let i = 0; i < edit.length; i++)
+			if (edit[i].hasAttribute("hidden")) edit[i].removeAttribute("hidden");
+			else edit[i].setAttribute("hidden", "hidden");
+}
+function removeButtons() {
+	let edit = document.getElementsByClassName('editButton');
+	let remove = document.getElementsByClassName('removeButton');
+	if (edit[0].hasAttribute("hidden"))
+		for (let i = 0; i < remove.length; i++)
+			if (remove[i].hasAttribute("hidden")) remove[i].removeAttribute("hidden");
+			else remove[i].setAttribute("hidden", "hidden");
+}
+
 window.initMap = () => {
 	let AzrieliLocation = { lat: 32.07458646100024, lng: 34.79189151265392 }
 	let map = new google.maps.Map(document.getElementById("map"), {
