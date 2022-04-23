0009ce2c691b9bf14158b900a3fb4cc1011bee1f
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-04-23 13:37:32 +0300

    add google maps support

--- modules/events/script.js
+++ modules/events/script.js
@@ -7,3 +7,17 @@ $(function() {
 			}
 		});
 });
+
+function initMap() {
+	let AzrieliLocation = { lat: 32.07458646100024, lng: 34.79189151265392 }
+	let map = new google.maps.Map(document.getElementById("map"), {
+		center: AzrieliLocation,
+		zoom: 16,
+	});
+	const marker = new google.maps.Marker({
+		position: AzrieliLocation,
+		map: map,
+	});
+}
+
+window.initMap = initMap;
