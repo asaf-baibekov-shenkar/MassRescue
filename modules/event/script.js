bed66ee4d111b87744bb35e45957d66a658dd88c
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-04-25 21:56:32 +0300

    change google maps to iframe

--- modules/event/script.js
+++ modules/event/script.js
@@ -32,17 +32,17 @@ function renderList() {
 }
 
 
-window.initMap = () => {
-	let AzrieliLocation = { lat: 32.07458646100024, lng: 34.79189151265392 }
-	let map = new google.maps.Map(document.getElementById("map"), {
-		center: AzrieliLocation,
-		zoom: 16,
-	});
-	const marker = new google.maps.Marker({
-		position: AzrieliLocation,
-		map: map,
-	});
-};
+// window.initMap = () => {
+// 	let AzrieliLocation = { lat: 32.07458646100024, lng: 34.79189151265392 }
+// 	let map = new google.maps.Map(document.getElementById("map"), {
+// 		center: AzrieliLocation,
+// 		zoom: 16,
+// 	});
+// 	const marker = new google.maps.Marker({
+// 		position: AzrieliLocation,
+// 		map: map,
+// 	});
+// };
 
 
 // types:
