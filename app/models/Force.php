1e7493ef967707ceee7c764b5412174f59f98557
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-07-09 13:29:25 +0300

    add event id foreign key nullable and fillable

--- app/models/Force.php
+++ app/models/Force.php
@@ -3,8 +3,11 @@
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Force extends Eloquent {
+	protected $primaryKey = 'force_id';
 	public $incrementing = true;
+	protected $nullable = ['event_id'];
 	public $force_id;
+	public $event_id;
 	public $title;
 	public $subtitle;
 	public $type;
@@ -12,7 +15,7 @@ class Force extends Eloquent {
 	public $longitude;
 
 	public $timestamps = true;
-	protected $fillable = ['title', 'subtitle', 'type', 'latitude', 'longitude'];
+	protected $fillable = ['event_id', 'title', 'subtitle', 'type', 'latitude', 'longitude'];
 }
 
 ?>
