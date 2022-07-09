34cd9cd932d343b674c3ee690ae1654532d30864
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-07-09 16:51:29 +0300

    change names of the inputs to adapt the request

--- app/views/events/modal.php
+++ app/views/events/modal.php
@@ -8,28 +8,29 @@
 				<div class="modal-body">
 					<div class="input-group mb-3">
 						<span class="input-group-text" id="basic-addon1">Event Name:</span>
-						<input type="text" class="form-control" id="InputEventName" name="event_name" required>
+						<input type="text" class="form-control" id="InputEventName" name="title" required>
 					</div>
 					<div class="input-group mb-3">
 						<label for="InputDescription" class="col-3 input-group-text">Description:</label>
 						<div class="col-9">
-							<textarea class="form-control" id="InputDescription" name="event_description"></textarea>
+							<textarea class="form-control" id="InputDescription" name="subtitle"></textarea>
 						</div>
 					</div>
 
 					<div class="input-group mb-3 justify-content-center">
 						<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-							<input type="radio" class="btn-check" name="event_type" id="fire_checkbox" value="2" required>
+							<input type="radio" class="btn-check" name="type" id="fire_checkbox" value="2" required>
 							<label class="btn btn-outline-primary" for="fire_checkbox">Fire</label>
 							
-							<input type="radio" class="btn-check" name="event_type" id="earthquake_checkbox" value="1" required>
+							<input type="radio" class="btn-check" name="type" id="earthquake_checkbox" value="1" required>
 							<label class="btn btn-outline-primary" for="earthquake_checkbox">Earthquake</label>
 							
-							<input type="radio" class="btn-check" name="event_type" id="other_checkbox" value="0" required checked>
+							<input type="radio" class="btn-check" name="type" id="other_checkbox" value="0" required checked>
 							<label class="btn btn-outline-primary" for="other_checkbox">Other</label>
 						</div>
 					</div>
 					<div id="map_form" class="w-100" style="height: 300px"></div>
+					<input type="hidden" name="id" value="">
 				</div>
 				<div class="modal-footer justify-content-evenly">
 					<button type="button" class="col-3 btn btn-danger" data-bs-dismiss="modal">Close</button>
