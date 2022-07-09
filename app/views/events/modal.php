2ab7ff542477ec560f9594b6c568b5c0c4fbd63b
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-07-09 15:07:04 +0300

    splite the event's modal into it's own file

---
+++ app/views/events/modal.php
@@ -0,0 +1,42 @@
+<div class="modal fade" id="event-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+	<form id="form-create-event" action="" method="get">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header justify-content-center">
+					<h5 class="modal-title" id="event-modal-title"></h5>
+				</div>
+				<div class="modal-body">
+					<div class="input-group mb-3">
+						<span class="input-group-text" id="basic-addon1">Event Name:</span>
+						<input type="text" class="form-control" id="InputEventName" name="event_name" required>
+					</div>
+					<div class="input-group mb-3">
+						<label for="InputDescription" class="col-3 input-group-text">Description:</label>
+						<div class="col-9">
+							<textarea class="form-control" id="InputDescription" name="event_description"></textarea>
+						</div>
+					</div>
+
+					<div class="input-group mb-3 justify-content-center">
+						<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+							<input type="radio" class="btn-check" name="event_type" id="fire_checkbox" value="2" required>
+							<label class="btn btn-outline-primary" for="fire_checkbox">Fire</label>
+							
+							<input type="radio" class="btn-check" name="event_type" id="earthquake_checkbox" value="1" required>
+							<label class="btn btn-outline-primary" for="earthquake_checkbox">Earthquake</label>
+							
+							<input type="radio" class="btn-check" name="event_type" id="other_checkbox" value="0" required checked>
+							<label class="btn btn-outline-primary" for="other_checkbox">Other</label>
+						</div>
+					</div>
+					<iframe width="100%" height="300px" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Tel%20Aviv%20azrieli+(Mass%20Rescue)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+					</iframe>
+				</div>
+				<div class="modal-footer justify-content-evenly">
+					<button type="button" class="col-3 btn btn-danger" data-bs-dismiss="modal">Close</button>
+					<button type="submit" class="col-3 btn btn-success" id="create_btn">Create</button>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
