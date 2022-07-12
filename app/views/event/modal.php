54884b972d735738d15570d6818f0a04f302aa83
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-07-12 18:42:57 +0300

    create new force from modal

--- app/views/event/modal.php
+++ app/views/event/modal.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="event-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
-	<form id="form-create-event" action="" method="get">
+	<form id="form-create-force" action="" method="get">
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
 				<div class="modal-header justify-content-center">
@@ -8,16 +8,23 @@
 				<div class="modal-body">
 					<div class="input-group mb-3">
 						<span class="input-group-text" id="basic-addon1">Force Name:</span>
-						<input type="text" class="form-control" id="InputEventName" name="event_name" required>
+						<input type="text" class="form-control" id="InputEventName" name="title" required>
 					</div>
 					<div class="input-group mb-3">
 						<label for="InputDescription" class="col-3 input-group-text">Message:</label>
 						<div class="col-9">
-							<textarea class="form-control" id="InputDescription" name="event_description"></textarea>
+							<textarea class="form-control" id="InputDescription" name="subtitle"></textarea>
 						</div>
 					</div>
-					<iframe width="100%" height="300px" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Tel%20Aviv%20azrieli+(Mass%20Rescue)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-					</iframe>
+					<div class="input-group mb-3">
+						<span class="input-group-text" id="basic-addon1">Force Location:</span>
+						<input type="text" class="form-control" id="InputAddress" name="">
+					</div>
+					<div id="map_form" class="w-100" style="height: 300px"></div>
+					<input type="hidden" name="event_id" value="">
+					<input type="hidden" name="type" value="">
+					<input type="hidden" name="latitude" value="">
+					<input type="hidden" name="longitude" value="">
 				</div>
 				<div class="modal-footer justify-content-evenly">
 					<button type="button" class="col-3 btn btn-danger" data-bs-dismiss="modal">Close</button>
--- public/js/event.js
+++ public/js/event.js
@@ -16,22 +16,56 @@ window.initMap = () => {
 				lng: parseFloat(force.longitude)
 			},
 			map: window.map,
+		});
 	});
+
+	let mapFormElement = document.getElementById("map_form");
+	window.map_form = new google.maps.Map(mapFormElement, {
+		center: {
+			lat: parseFloat(event.latitude),
+			lng: parseFloat(event.longitude)
+		},
+		zoom: 14
+	});
+
+	window.formMapMarkers = [];
+	window.map_form.addListener("click", (mapsMouseEvent) => {
+		window.formMapMarkers.map(marker => { marker.setMap(null) });
+		window.formMapMarkers = [];
+		let clickedLocation = mapsMouseEvent.latLng.toJSON();
+		window.formMapMarkers.push( new google.maps.Marker({ map: map_form, position: clickedLocation }) );
+		document.getElementById("InputAddress").value = `${clickedLocation.lat.toFixed(5)}, ${clickedLocation.lng.toFixed(5)}`;
+	});
+
+	let mapFormAutocompleteElement = document.getElementById("InputAddress");
+	window.searchBox = new google.maps.places.SearchBox(mapFormAutocompleteElement);
+	window.searchBox.addListener("places_changed", () => {
+		const places = window.searchBox.getPlaces();
+		if (places.length == 0) return;
+		window.formMapMarkers.forEach((marker) => { marker.setMap(null); });
+		window.formMapMarkers = [];
+		const bounds = new google.maps.LatLngBounds();
+		places.forEach((place) => {
+			if (!place.geometry || !place.geometry.location) {
+				console.log("Returned place contains no geometry");
+				return;
+			}
+			window.formMapMarkers.push( new google.maps.Marker({ map: map_form, title: place.name, position: place.geometry.location }) );
+			if (place.geometry.viewport)
+				bounds.union(place.geometry.viewport);
+			else
+				bounds.extend(place.geometry.location);
+		});
+		map_form.fitBounds(bounds);
 	});
 };
 
 $(function() {
-	$('#filter .btn').click(function(event) {
+	$('#filter .btn').click(function(e) {
 		let type = $(this).attr("id").substring(4);
-		type = type.charAt(0).toUpperCase() + type.slice(1);
-		$('#event-modal').on('show.bs.modal', () => {
-			$('#event-modal-title').html(`Add ${type}`);
-			$('#create_btn').html("Submit");
-			$('#InputEventName').val('');
-			$('#InputDescription').val('');
-			$(`input[name="event_type"][value="0"]`).prop('checked', true);
-		})
-		$('#event-modal').modal('show');
+		let latitude = event.latitude
+		let longitude = event.longitude
+		showModal(event.event_id, type, latitude, longitude)
 	});
 	$('.cell').hover(function () {
 		let index = $(this).attr('index');
@@ -39,4 +73,41 @@ $(function() {
 		if (typeof google === 'object' && typeof google.maps === 'object')
 			window.map.panTo({ lat: parseFloat(force.latitude), lng: parseFloat(force.longitude) });
 	}, function () { });
+	$('#form-create-force').submit(function(e) {
+		e.preventDefault();
+		if (window.formMapMarkers.length > 0) {
+			let marker = window.formMapMarkers[0]
+			$(`input[name="latitude"]`).val(marker.position.lat);
+			$(`input[name="longitude"]`).val(marker.position.lng);
+		}
+		console.log($("#form-create-force").serializeArray());
+		$.ajax({
+			type: "POST",
+			url: window.location.href.split('?')[0] + '/create',
+			data: $("#form-create-force").serializeArray(),
+			success: function (data) {
+				location.reload()
+			},
+			error: function (data) {
+				alert("error");
+			}
+		});
+	})
 });
+
+function showModal(event_id, type, latitude, longitude) {
+	$('#event-modal').on('show.bs.modal', () => {
+		$('#event-modal-title').html(`Add ${type.charAt(0).toUpperCase() + type.slice(1)}`);
+		$(`input[name="event_id"]`).val(event_id);
+		$(`input[name="type"]`).val(type);
+		$('#InputEventName').val('');
+		$('#InputDescription').val('');
+		$('#create_btn').html("Submit");
+		if (typeof google === 'object' && typeof google.maps === 'object') {
+			window.formMapMarkers.forEach(marker => { marker.setMap(null); });
+			window.map_form.setZoom(12)
+			window.map_form.setCenter({ lat: parseFloat(latitude), lng: parseFloat(longitude) });
+		}
+	})
+	$('#event-modal').modal('show');
+}
