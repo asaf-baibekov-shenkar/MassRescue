41c41bf9cf93b5228e102e02f47258299aba08f9
OriBashary1 <100477565+OriBashary1@users.noreply.github.com>
2022-07-14 12:04:30 +0300

    fix the href

--- app/views/event/index.php
+++ app/views/event/index.php
@@ -60,7 +60,7 @@
 			</div>
 			<div class="offcanvas-body d-flex flex-column justify-content-between gap-5 p-0">
 				<div class="d-flex flex-column gap-2">
-					<a class="nav-link p-4 fs-3 bg-secondary d-flex align-items-center gap-2" href="../events/index.html">
+					<a class="nav-link p-4 fs-3 bg-secondary d-flex align-items-center gap-2" href="./events/index.php">
 						<svg width="40" height="40" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 							<path d="M19 4h-1V3c0-.55-.45-1-1-1s-1 .45-1 1v1H8V3c0-.55-.45-1-1-1s-1 .45-1 1v1H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 15c0 .55-.45 1-1 1H6c-.55 0-1-.45-1-1V9h14v10zM7 11h2v2H7zm4 0h2v2h-2zm4 0h2v2h-2z"></path>
 						</svg>
